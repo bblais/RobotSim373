e93f4de6dd2ccb4248c5a3e751ad75e7d6eb4405
Brian Blais <bblais@gmail.com>
2020-10-30 15:12:12 -0400

    end simulation bug

--- RobotSim373/__init__.py
+++ RobotSim373/__init__.py
@@ -1,4 +1,4 @@
 from .robotsim import *
 
-__version__='0.0.20'
+__version__='0.0.21'
 print("Version: ",__version__)
--- RobotSim373/robotsim.py
+++ RobotSim373/robotsim.py
@@ -264,6 +264,8 @@ class StateMachine(UserDict):
     def __init__(self,*args,first_state=None):
         super(StateMachine, self).__init__(*args)
         self.first_state=first_state
+        self['_end_simulation']=(None,None)
+
         if self.first_state not in self:
             print("The first state not found is: ",self.first_state)
             print("The available states are: ",list(self.keys()))
@@ -271,6 +273,7 @@ class StateMachine(UserDict):
             raise KeyError("'%s'" % self.first_state)
 
         for state in self.keys():
+
             actions,next_states=self[state]
             if isinstance(next_states,str):
                 try:
